44566d1cd29ca9d69557f290eb73e33ac2f08a99
Ayman-shaim1 <ayman.shaim10@gmail.com>
2023-05-02 14:43:03 +0000

    keep working on navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Nav,
   NavBar,
@@ -8,56 +8,61 @@ import {
   NavDropdown,
   NavDropdownItem,
   NavDropdownMenu,
+  NavBtnToggleMenu,
 } from "./styles/Header.styled";
 
 const Header = () => {
+  const [showNavBar, setShowNavBar] = useState(false);
+
   return (
     <NavBar>
-      <NavLogoLink to='/'>Afousse</NavLogoLink>
-      <Nav>
-        <NavItem to='/home'>Home</NavItem>
-        <NavItem to='/shop'>Shop</NavItem>
-        <NavItem to='/shop/men'>Men</NavItem>
-        <NavItem to='/shop/men'>Women</NavItem>
-        <NavItem to='/shop/men'>Contact</NavItem>
+      <NavLogoLink to="/">Afousse</NavLogoLink>
+      <Nav show={showNavBar}>
+        <NavItem to="/home">Home</NavItem>
+        <NavItem to="/shop">Shop</NavItem>
+        <NavItem to="/shop/men">Men</NavItem>
+        <NavItem to="/shop/men">Women</NavItem>
+        <NavItem to="/shop/men">Contact</NavItem>
       </Nav>
       <Nav>
         <NavItemIcon>
-          <i className='bi bi-search'></i>
+          <i className="bi bi-search"></i>
         </NavItemIcon>
         <NavItemIcon>
-          <i className='bi bi-person'></i>
+          <i className="bi bi-person"></i>
         </NavItemIcon>
         <NavItemIcon>
-          <i className='bi bi-bag'></i>
+          <i className="bi bi-bag"></i>
         </NavItemIcon>
         <NavItemIcon>
-          <i className='bi bi-heart'></i>
+          <i className="bi bi-heart"></i>
         </NavItemIcon>
         <NavDropdown>
           <NavItemIcon>
-            <i className='bi bi-globe'></i>
+            <i className="bi bi-globe"></i>
             <NavDropdownMenu>
-              <NavDropdownItem to='/english'>English</NavDropdownItem>
-              <NavDropdownItem to='/arabe'>Arabe</NavDropdownItem>
+              <NavDropdownItem to="/english">English</NavDropdownItem>
+              <NavDropdownItem to="/arabe">Arabe</NavDropdownItem>
             </NavDropdownMenu>
           </NavItemIcon>
         </NavDropdown>
       </Nav>
       <Nav>
-      
         <NavItemIcon>
-          <i className='bi bi-bag'></i>
+          <i className="bi bi-bag"></i>
         </NavItemIcon>
-        <NavDropdown>
-          <NavItemIcon>
-            <i className='bi bi-globe'></i>
+        <NavItemIcon>
+          <NavDropdown>
+            <i className="bi bi-globe"></i>
             <NavDropdownMenu>
-              <NavDropdownItem to='/english'>English</NavDropdownItem>
-              <NavDropdownItem to='/arabe'>Arabe</NavDropdownItem>
+              <NavDropdownItem to="/english">English</NavDropdownItem>
+              <NavDropdownItem to="/arabe">Arabe</NavDropdownItem>
             </NavDropdownMenu>
-          </NavItemIcon>
-        </NavDropdown>
+          </NavDropdown>
+        </NavItemIcon>
+        <NavBtnToggleMenu onClick={() => setShowNavBar(!showNavBar)}>
+          <i className="bi bi-list"></i>
+        </NavBtnToggleMenu>
       </Nav>
     </NavBar>
   );
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -12,44 +12,48 @@ const progressAnimation = keyframes`
 `;
 
 export const NavBar = styled.nav`
-  background-color: var(--white-color);
+  background: transparent;
   display: flex;
   align-content: center;
   justify-content: space-around;
-  padding: 20px 0;
+  padding: 30px 0;
   width: 100%;
-  border-bottom: 1px solid var(--light-gray-color);
-  box-shadow: 1px 1px 2px 1px var(--light-gray-color);
+
+  & > :nth-child(4) {
+    display: none;
+  }
+
   @media (max-width: 1200px) {
-    & > :nth-child(2),
-    > :nth-child(3) {
+    justify-content: space-between;
+    & > :nth-child(2) {
+      flex-direction: column;
+      display: ${(props) => (props.show ? "flex" : "none")};
+    }
+    & > :nth-child(3) {
       flex-direction: column;
       display: none;
     }
     & > :nth-child(4) {
-   
       display: flex;
     }
   }
 `;
 
-export const Nav = styled.ul`
-  list-style: none;
+export const Nav = styled.div`
   display: flex;
-  align-content: center;
   justify-content: center;
+  align-items: center;
 `;
 
-const StyledNavItem = styled.li`
+export const NavItem = styled(Link)`
   margin: 0 10px;
-`;
-
-export const NavLink = styled(Link)`
   color: var(--black-color);
   text-decoration: none;
   text-transform: uppercase;
+  text-align: center;
   font-weight: bold;
   transition: 0.1s;
+
   cursor: pointer;
   &::after {
     content: "";
@@ -59,9 +63,11 @@ export const NavLink = styled(Link)`
     margin-top: 2px;
     background-color: var(--primary-color);
   }
+
   &:hover {
     color: var(--secondary-color);
   }
+
   &:hover::after {
     width: 100%;
     animation: ${progressAnimation} 0.5s linear;
@@ -75,6 +81,7 @@ export const NavLogoLink = styled(Link)`
   color: var(--black-color);
   text-decoration: none;
   cursor: pointer;
+  padding: 0;
 `;
 
 export const NavLogo = styled.img`
@@ -83,22 +90,13 @@ export const NavLogo = styled.img`
   border-radius: 50%;
 `;
 
-export const NavItem = ({ children, to }) => (
-  <StyledNavItem>
-    <NavLink to={to}>{children}</NavLink>
-  </StyledNavItem>
-);
-
-const StyledNavItemIcon = styled.li`
+export const NavItemIcon = styled.a`
   margin: 0 12px;
-  font-size: 16px;
-`;
-
-const NavLinkIcon = styled.a`
+  font-size: 18px;
+  font-weight: bold;
   color: var(--dark-gray-color);
   text-decoration: none;
   text-transform: uppercase;
-  font-weight: bold;
   transition: 0.1s;
   cursor: pointer;
   &:hover {
@@ -106,35 +104,25 @@ const NavLinkIcon = styled.a`
   }
 `;
 
-export const NavItemIcon = ({ children, to }) => (
-  <StyledNavItemIcon>
-    <NavLinkIcon to={to}>{children}</NavLinkIcon>
-  </StyledNavItemIcon>
-);
-
-const StyledNavDropdown = styled.div`
+export const NavDropdown = styled.div`
   position: relative;
   &:hover div {
     display: block;
   }
-  &::after {
-  }
 `;
-export const NavDropdown = ({ children }) => (
-  <StyledNavDropdown>{children}</StyledNavDropdown>
-);
+
 export const NavDropdownMenu = styled.div`
   position: absolute;
+  right: -60%;
   display: none;
   width: 150px;
   border: 1px solid var(--gray-color);
   padding: 0;
   margin-top: 5px;
-  transform: translateX(-30%);
+  /* transform: translateX(-30%); */
   background-color: #fff;
   z-index: 1000;
 `;
-
 export const NavDropdownItem = styled(Link)`
   font-size: 12px;
   font-weight: 500;
@@ -146,5 +134,23 @@ export const NavDropdownItem = styled(Link)`
   }
   &:last-child {
     border-bottom: none;
+  }
+`;
+
+export const NavBtnToggleMenu = styled.button`
+  display: block;
+  font-size: 20px;
+  color: var(--dark-gray-color);
+  /* background-color: var(--white-color); */
+  background: transparent;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+
+  border: none;
+  cursor: pointer;
+  /* padding: 10px 15px; */
+  &:hover {
+    background-color: var(--light-gray-color);
   }
 `;
